c339c53fe0cacb811bed76f8fa4b5b0b8d30088c
Ryan Posener <ryan.posener@agilethought.com>
2020-09-29 12:20:18 -0600

    Add VueHelper and Instructions

--- README.md
+++ README.md
@@ -1,2 +1,92 @@
 # DotNetVueIntegration
 Integration Point to run Vue (via Vite server hot reload) in the SPA framework within DOTNET5.
+
+## To get started
+
+1. Create a new ASP.NET 5 SPA Application
+1. Remove the `/ClientApp` directory
+1. Use `npm init vite` to create a new `vue` app in the `/ClientApp` directory
+1. Add `VueHelper.cs` file to your project (Nuget Package coming soon...)
+1. Modify Startup.cs
+   1. Add the line `spa.UseViteDevelopmentServer();` in it place of the current dev server for the SPA Builder.
+
+Check your .csproj file for the following `<Target>` sections:
+
+*  Remove unnecessary sections (e.g. for Angular or React)
+*  Added `<Target Name="BuildVue">` section for release
+*  Added `<Target Name="PublishRunVite">` section 
+
+```xml
+<Project Sdk="Microsoft.NET.Sdk.Web">
+
+	<PropertyGroup>
+		<TargetFramework>net5.0</TargetFramework>
+		<TypeScriptCompileBlocked>true</TypeScriptCompileBlocked>
+		<TypeScriptToolsVersion>Latest</TypeScriptToolsVersion>
+		<IsPackable>false</IsPackable>
+		<SpaRoot>ClientApp\</SpaRoot>
+		<DefaultItemExcludes>$(DefaultItemExcludes);$(SpaRoot)node_modules\**</DefaultItemExcludes>
+
+		<!-- Set this to true if you enable server-side prerendering -->
+		<BuildServerSideRenderer>false</BuildServerSideRenderer>
+	</PropertyGroup>
+
+	<ItemGroup>
+		<PackageReference Include="Microsoft.AspNetCore.SpaServices.Extensions" Version="5.0.0-rc.1.20451.17" />
+	</ItemGroup>
+
+	<ItemGroup>
+		<!-- Don't publish the SPA source files, but do show them in the project files list -->
+		<Content Remove="$(SpaRoot)**" />
+		<None Remove="$(SpaRoot)**" />
+		<None Include="$(SpaRoot)**" Exclude="$(SpaRoot)node_modules\**" />
+	</ItemGroup>
+
+	<ItemGroup>
+		<Folder Include="wwwroot\" />
+	</ItemGroup>
+
+	<Target Name="DebugEnsureNodeEnv" BeforeTargets="Build" Condition=" '$(Configuration)' == 'Debug' And !Exists('$(SpaRoot)node_modules') ">
+		<!-- Ensure Node.js is installed -->
+		<Exec Command="node --version" ContinueOnError="true">
+			<Output TaskParameter="ExitCode" PropertyName="ErrorCode" />
+		</Exec>
+		<Error Condition="'$(ErrorCode)' != '0'" Text="Node.js is required to build and run this project. To continue, please install Node.js from https://nodejs.org/, and then restart your command prompt or IDE." />
+		<Message Importance="high" Text="Restoring dependencies using 'npm'. This may take several minutes..." />
+		<Exec WorkingDirectory="$(SpaRoot)" Command="npm install" />
+	</Target>
+
+	<Target Name="BuildVue" BeforeTargets="Build" Condition=" '$(Configuration)' == 'Release'">
+		<!-- Ensure Node.js is installed -->
+		<Exec Command="node --version" ContinueOnError="true">
+			<Output TaskParameter="ExitCode" PropertyName="ErrorCode" />
+		</Exec>
+		<Error Condition="'$(ErrorCode)' != '0'" Text="Node.js is required to build and run this project. To continue, please install Node.js from https://nodejs.org/, and then restart your command prompt or IDE." />
+		<Message Importance="high" Text="Restoring dependencies using 'npm'. This may take several minutes..." />
+		<Exec WorkingDirectory="$(SpaRoot)" Command="npm run build" />
+	</Target>
+
+	<Target Name="PublishRunVite" AfterTargets="ComputeFilesToPublish">
+		<!-- As part of publishing, ensure the JS resources are freshly built in production mode -->
+		<Exec WorkingDirectory="$(SpaRoot)" Command="npm install" />
+		<Exec WorkingDirectory="$(SpaRoot)" Command="npm run build" />
+		<Exec WorkingDirectory="$(SpaRoot)" Command="npm run build --ssr" Condition=" '$(BuildServerSideRenderer)' == 'true' " />
+
+		<!-- Include the newly-built files in the publish output -->
+		<ItemGroup>
+			<DistFiles Include="$(SpaRoot)dist\**; $(SpaRoot)dist-server\**" />
+			<DistFiles Include="$(SpaRoot)node_modules\**" Condition="'$(BuildServerSideRenderer)' == 'true'" />
+			<ResolvedFileToPublish Include="@(DistFiles->'%(FullPath)')" Exclude="@(ResolvedFileToPublish)">
+				<RelativePath>%(DistFiles.Identity)</RelativePath>
+				<CopyToPublishDirectory>PreserveNewest</CopyToPublishDirectory>
+				<ExcludeFromSingleFile>true</ExcludeFromSingleFile>
+			</ResolvedFileToPublish>
+		</ItemGroup>
+	</Target>
+
+</Project>
+```
+
+NOTES:
+
+* On first run, the helper will export your local asp.net dev certificate to the Vite Hosted Vue app and create a devcert.pfx file and vite.config.js file.
---
+++ ViteHosted/VueHelper.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace ViteHosted
+{
+    public static class VueHelper
+    {
+        // done message of 'npm run serve' command.
+        private static string DoneMessage { get; } = "Dev server running at:";
+
+        /// <summary>
+        /// Adds Connection to Vite Hosted VueApplication
+        /// configured per <seealso cref="SpaOptions"/> on the <paramref name="spa"/>.
+        /// NOTE: (this will create devcert.pfx and vite.config.js in your Vue Application on first run)
+        /// </summary>
+        /// <param name="spa"></param>
+        public static void UseViteDevelopmentServer(this ISpaBuilder spa)
+        {
+            // Default HostingPort
+            if (spa.Options.DevServerPort == 0)
+                spa.Options.DevServerPort = 3000;
+
+            var devServerEndpoint = new Uri($"https://localhost:{spa.Options.DevServerPort}");
+            var loggerFactory = spa.ApplicationBuilder.ApplicationServices.GetService<ILoggerFactory>();
+            var webHostEnvironment = spa.ApplicationBuilder.ApplicationServices.GetService<IWebHostEnvironment>();
+            var logger = loggerFactory.CreateLogger("Vue");
+
+            // Is this already running
+            bool IsRunning = IPGlobalProperties.GetIPGlobalProperties()
+                .GetActiveTcpListeners()
+                .Select(x => x.Port)
+                .Contains(spa.Options.DevServerPort);
+
+            if (!IsRunning)
+            {
+                var isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+
+                // export dev cert
+                var tempDir = webHostEnvironment.ContentRootPath;
+                var tempPfx = Path.Combine(tempDir, spa.Options.SourcePath, "devcert.pfx");
+                var tempConfig = Path.Combine(tempDir, spa.Options.SourcePath, "vite.config.js");
+
+                if (!File.Exists(tempPfx) || !File.Exists(tempConfig))
+                {
+                    var pfxPassword = Guid.NewGuid().ToString("N");
+                    logger.LogInformation($"Exporting dotnet dev cert to {tempPfx} for Vite");
+                    logger.LogDebug($"Export password: {pfxPassword}");
+                    var certExport = new ProcessStartInfo
+                    {
+                        FileName = isWindows ? "cmd" : "dotnet",
+                        Arguments = $"{(isWindows ? "/c dotnet " : "")}dev-certs https -v -ep {tempPfx} -p {pfxPassword}",
+                        RedirectStandardError = true,
+                        RedirectStandardInput = true,
+                        RedirectStandardOutput = true,
+                        UseShellExecute = false,
+                    };
+                    var exportProcess = Process.Start(certExport);
+                    exportProcess.WaitForExit();
+                    if (exportProcess.ExitCode == 0)
+                    {
+                        logger.LogInformation(exportProcess.StandardOutput.ReadToEnd());
+                    }
+                    else
+                    {
+                        logger.LogError(exportProcess.StandardError.ReadToEnd());
+                    }
+
+                    // create config
+                    File.WriteAllText(tempConfig, $"export default {{\r\nhttps:true,\r\nhttpsOptions: {{\r\npfx: '{Path.GetFileName(tempPfx)}',\r\npassphrase: '{pfxPassword}'\r\n}}\r\n}}");
+                    logger.LogInformation($"Creating Vite config: {tempConfig}");
+                }
+
+                // launch vue.js development server
+                var processInfo = new ProcessStartInfo
+                {
+                    FileName = isWindows ? "cmd" : "npm",
+                    Arguments = $"{(isWindows ? "/c npm " : "")}run dev",
+                    WorkingDirectory = spa.Options.SourcePath,
+                    RedirectStandardError = true,
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                };
+                var process = Process.Start(processInfo);
+                var tcs = new TaskCompletionSource<int>();
+                _ = Task.Run(() =>
+                {
+                    try
+                    {
+                        string line;
+                        while ((line = process.StandardOutput.ReadLine()?.Trim()) != null)
+                        {
+                            if (!String.IsNullOrEmpty(line))
+                            {
+                                logger.LogInformation(line);
+                                if (!tcs.Task.IsCompleted && line.Contains(DoneMessage))
+                                {
+                                    tcs.SetResult(1);
+                                }
+                            }
+                        }
+                    }
+                    catch (EndOfStreamException ex)
+                    {
+                        logger.LogError(ex.ToString());
+                        tcs.SetException(new InvalidOperationException("'npm run dev' failed.", ex));
+                    }
+                });
+                _ = Task.Run(() =>
+                {
+                    try
+                    {
+                        string line;
+                        while ((line = process.StandardError.ReadLine()?.Trim()) != null)
+                        {
+                            logger.LogError(line);
+                        }
+                    }
+                    catch (EndOfStreamException ex)
+                    {
+                        logger.LogError(ex.ToString());
+                        tcs.SetException(new InvalidOperationException("'npm run dev' failed.", ex));
+                    }
+                });
+
+                if (!tcs.Task.Wait(spa.Options.StartupTimeout))
+                {
+                    throw new TimeoutException();
+                }
+            }
+            spa.UseProxyToSpaDevelopmentServer(devServerEndpoint);
+        }
+    }
+
+}
